c95a6ac79145a0c146e8d850ed21f2d34af7bfc5
JuliaKudryavtseva <67862423+JuliaKudryavtseva@users.noreply.github.com>
2023-03-25 22:47:58 +0300

    Add files via upload

---
+++ few_shot/get_cuda_device.py
@@ -0,0 +1,9 @@
+def _get_cuda_command(cuda_device_nvidia):
+    _cuda = {'cuda:5': 'cuda:3',
+             'cuda:1': 'cuda:4',
+             'cuda:2': 'cuda:5',
+             'cuda:0': 'cuda:0',
+             'cuda:3': 'cuda:1',
+             'cuda:4': 'cuda:2'}
+    _device = _cuda[cuda_device_nvidia][-1]
+    return f'os.environ["CUDA_VISIBLE_DEVICES"]="{_device}"'
